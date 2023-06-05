a7d8b2789c44a91552c57912c06ddef7bb93ae7c
implObserver <prognozman@mail.ru>
2023-06-05 16:51:31 +0500

    feat:add popup

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 const bookshelf = document.querySelector('.books');
 const inputsReadPages = document.querySelectorAll('.pages-read');
-const myDomLibrary = new Array();
+const bookPlace = document.querySelector('.book-place');
+const popup = document.querySelector('.popup');
+const form = document.querySelector('.create-book');
 const myLibrary = [];
 
-function Book(author, title, pages, status) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.status = status;
-}
+class Book {
+    constructor(author, title, pages, status) {
+        this.numPages = pages;
 
-let book1 = new Book('I.Efremov', 'Since fantastic', 304, true);
-let book2 = new Book('A.Uriev', 'My strange chess', 5, false);
-let book3 = new Book('Anonimus', 'Anonimus', 123, false);
-
-myLibrary.push(book1, book2, book3);
-
-class DomBook {
-    constructor() {
         this.book = createElement('book');
         this.spine = createElement('spine');
         this.cover = createElement('cover');
@@ -32,12 +23,25 @@ class DomBook {
         this.pages = createElement('pages');
         this.readPages = document.createElement('input');
         this.readPages.classList.add('pages-read');
-        this.readPages.value = '0';
-        this.readPages.type = 'text';
+        this.readPages.type = 'number';
         this.allPages = document.createElement('span');
         this.allPages.classList.add('all-pages');
 
+        this.readPages.value = 0;
+
+        this.allPages.textContent = `/${pages}`;
+        this.author.textContent = author;
+        this.title.textContent = title;
+        this.label.className = 'unchecked';
+        
+        this.lastReadPages = '0';
+        if (status) {
+            this.label.className = 'checked';
+            this.readPages.value = pages;
+        }
+
         this.addBookStructure();
+        this.addBookListeners();
     }
 
     addBookStructure() {
@@ -48,6 +52,19 @@ class DomBook {
         appendChildrens(this.pages, this.readPages, this.allPages);
         this.bookHeader.appendChild(this.bookMarkWrapper);
         this.bookMarkWrapper.appendChild(this.label);
+    }
+
+    addBookListeners() {
+        this.label.addEventListener('click', e => {
+            if (this.label.className === 'unchecked') {
+                this.label.className = 'checked';
+                this.lastReadPages = this.readPages.value;
+                this.readPages.value = this.numPages;
+            } else {
+                this.label.className = 'unchecked';
+                this.readPages.value = this.lastReadPages;
+            }
+        });
     }
 }
 
@@ -63,45 +80,35 @@ function appendChildrens(element, ...childrens) {
     }
 }
 
-function addBookToLibrary() {
-    for (let book of myLibrary) {
-        const domBook = new DomBook();
-        domBook.allPages.textContent = book.pages;
-        domBook.author.textContent = book.author;
-        domBook.title.textContent = book.title;
-        domBook.label.className = book.status ? 'checked'
-            : 'unchecked';
-        domBook.lastReadPages = '0/';
-        
-        if (book.status) {
-            domBook.readPages.value = book.pages;
-        }
-
-        domBook.readPages.value += `/`;
-
-        bookshelf.appendChild(domBook.book);
-        myDomLibrary.push(domBook);
-    }
+function addBookToLibrary(domBook) {
+    myLibrary.push(domBook);
 }
 
-addBookToLibrary();
-
-const books = document.querySelectorAll('.book');
-
-for (let domBook of myDomLibrary) {
-
-    domBook.label.addEventListener('click', e => {
-        if (domBook.label.className === 'unchecked') {
-            domBook.label.className = 'checked';
-            domBook.lastReadPages = domBook.readPages.value;
-            domBook.readPages.value = `${domBook.allPages.textContent}/`;
-        } else {
-            domBook.label.className = 'unchecked';
-            domBook.readPages.value = domBook.lastReadPages;
-        }
-    });
-
-    domBook.trashWrapper.addEventListener('click', e => {
-        domBook.trashWrapper.style.backgroundColor = 'red';
-    })
+function viewBook(book) {
+    bookshelf.appendChild(book.book);
 }
+
+form.addEventListener('click', e => {
+    e.stopPropagation();
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let author = document.querySelector('.book-author');
+    let title = document.querySelector('.book-title');
+    let pages = document.querySelector('.book-pages');
+    let status = document.querySelector('.is-read');
+    let book = new Book(author.value, title.value, pages.value, status.checked);
+    addBookToLibrary(book);
+    viewBook(book);
+})
+
+bookPlace.addEventListener('click', e => {
+    popup.style.visibility = 'visible';
+    popup.style.opacity = 1;
+})
+
+popup.addEventListener('click', e => {
+    popup.style.visibility = 'hidden';
+    popup.style.opacity = 0;
+})
